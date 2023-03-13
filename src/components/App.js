ffa4d33bc2222a799e608c36ebd68aec90e9e421
Daphineatim <hopedaphine19@gmail.com>
2023-03-13 09:46:23 -0700

    Fix eslint linter errors

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Greeting from './Greeting';
--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchGreetingApi } from '../redux/greetings/greetings'
+import { fetchGreetingApi } from '../redux/greetings/greetings';
 
 const Greeting = () => {
   const dispatch = useDispatch();
@@ -10,10 +10,10 @@ const Greeting = () => {
   const message = useSelector((state) => state.greetingsReducer);
 
   return (
-  <div className="bg-slate-200 mt-20 flex flex-col items-center my-4 mx-12 border p-5">
-  <p className="text-2xl">{ message }</p>
-</div>
-);
+    <div className="bg-slate-200 mt-20 flex flex-col items-center my-4 mx-12 border p-5">
+      <p className="text-2xl">{ message }</p>
+    </div>
+  );
 };
 
 export default Greeting;
